aeeb8dce8f042c6cea646f5268c3c80782308263
andres9906 <stivenaereo@gmail.com>
2020-02-17 15:28:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/recycleview/MyUserRecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.recycleview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.data.User
+
+class MyUserRecyclerViewAdapter(private val nvalue: List<User>) : RecyclerView.Adapter<MyUserRecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    inner class ViewHolder(val nview: View) : {
+
+    }
+}
